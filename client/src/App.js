de7bba6edb87505e83bf009706153f4f88e5e40d
Jonathan Kohen <jonathan.a.kohen@gmail.com>
2022-11-25 20:58:23 -0500

    Started working on client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				{/* <Navbar /> */}
+				<Switch>
+					<Route exact path="/">
+						<Reg />
+					</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/musicians">
+						<Main />
+					</Route>
+					{/* <Route path="/about">
+						<About />
+					</Route>
+					<Route path="/user/:id">
+						<Show />
+					</Route>
+					<Route path="/edit/:id">
+						<Edit user={user} />
+					</Route> */}
+				</Switch>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
